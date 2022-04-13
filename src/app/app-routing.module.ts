056ffd0fa5ede2e48acbeddd79ef102802b603c6
ahmedazouzi <ahmedazouzi97@gmail.com>
2022-04-13 03:29:43 +0100

    generate a few new component of our layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminloginComponent } from './adminComponents/adminlogin/adminlogin.component';
+import { HeaderComponent } from './header/header.component';
+import { UserloginComponent } from './userComponents/userlogin/userlogin.component';
+import { UserregistrationComponent } from './userComponents/userregistration/userregistration.component';
 
 
+const routes:Routes=[{path:"account/Userlogin",component:UserloginComponent},
+{path:"register", component:UserregistrationComponent},
+{path:"hh",component:HeaderComponent},
+{path:"adminlogin",component:AdminloginComponent}
 
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports:[RouterModule],
+  imports: [RouterModule.forRoot(routes)
+    
   ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { UserloginComponent } from './userComponents/userlogin/userlogin.component';
+import { UserregistrationComponent } from './userComponents/userregistration/userregistration.component';
+import { AdminloginComponent } from './adminComponents/adminlogin/adminlogin.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PathsComponent } from './paths/paths.component';
+import { CoursesComponent } from './courses/courses.component';
+import { EventsComponent } from './events/events.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserloginComponent,
+    UserregistrationComponent,
+    AdminloginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PathsComponent,
+    CoursesComponent,
+    EventsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
