197be3ddf44eb987c620ef670ad0296df72473b1
ahmeddazouzi <59941927+ahmeddazouzi@users.noreply.github.com>
2022-04-25 22:42:54 +0100

    Delete src directory

--- src/app/admin.ts
+++
@@ -1,6 +0,0 @@
-export class Admin {
-      id:number  ;
-      email:String ;
-      name :String;
-      password:String ;
-}
--- src/app/course.ts
+++
@@ -1,9 +0,0 @@
-export class Course {
-       id: number ;
-       name: String ;
-      category:String ;
-      descriptionCourse:String ;
-      imgUrlCourse:String ;
-}
-
-
